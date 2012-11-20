eb795ae3646b0beeafa355b1688e882c861517fc
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-20 13:57:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datester::Application.config.secret_token = 'e9f09e78977b06ff3a2c33a7c5c6208f70c18a9325d6afb668dc2e73924e9c29fb58ae7278ca7656c12321b14720aaef0e5d08cce67f467393dae86e6e35f752'
