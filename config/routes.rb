7c86ac9772907a641e4e48d3663d3d48eced59e3
Jonathan Ku <jonathanku88@gmail.com>
2012-11-21 13:44:49 -0500

    my branch, login

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Datester::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'session#new'
 
   # See how all your routes lay out with "rake routes"
 
