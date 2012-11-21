605e28aa3ac794f14108c3ccd0cc07bc5f484f36
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-21 10:16:03 -0500

    added navbar for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120232427) do
+ActiveRecord::Schema.define(:version => 20121121034359) do
 
   create_table "matches", :force => true do |t|
     t.boolean  "matched"
