d9c23eef44ac18b2eca32a8f4e5b88123bce1228
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-23 15:09:05 -0500

    finally fixed avatar photos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,4 @@
 class DashboardController < ApplicationController
     def index
-      if @auth.photos.where(:is_primary => true).first.image.thumb.url == true
-        @avatar = @auth.photos.where(:is_primary => true).first.image.thumb.url
-      else
-        @avatar = nil
-      end
     end
 end
