a17bad988026cc56452c28e2d69490e8df7261c8
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-25 14:19:08 -0500

    working on matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -10,11 +10,16 @@ class MatchesController < ApplicationController
   end
 
   def grade
+
+    @receiver_id = Question.find(params[:question_id]).user_id
+    @sender_id = @auth[:id]
+
     correct = Answer.where(:question_id => params[:question_id], :is_correct => "on").first.answer_text
     select = params[:select]
 
     if select == correct
       @attempt = "success"
+      # Match.create(:matched => true, :receiver_id => , :sender_id =>  )
     else
       @attempt = "failure"
 
